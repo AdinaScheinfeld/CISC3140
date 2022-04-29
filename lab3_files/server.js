1fed22e8fecd3230fda78e79e7d60aaa339c3807
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-04-29 18:00:41 -0400

    Add functionality that shows all results per class

--- lab3_files/server.js
+++ lab3_files/server.js
@@ -26,11 +26,21 @@ app.get("/", (req, res, next) => {
 });
 
 //endpoint to get a list of cars in the Cars table
+/*
+    Car_ID, Year, Make, Model, 
+    Racer_Turbo, Racer_Supercharged, Racer_Performance, Racer_Horsepower, 
+    Car_Overall, 
+    Engine_Modifications, Engine_Performance, Engine_Chrome, Engine_Detailing, Engine_Cleanliness, 
+    Body_Frame_Undercarriage, Body_Frame_Suspension, Body_Frame_Chrome, Body_Frame_Detailing, Body_Frame_Cleanliness, 
+    Mods_Paint, Mods_Body, Mods_Wrap, Mods_Rims, Mods_Interior, Mods_Other, Mods_ICE, Mods_Aftermarket, Mods_WIP, and Mods_Overall
+    queries can optionally be specified to display only records that meet certain criteria
+*/
 app.get("/api/cars", (req, res, next) => {
 
     // initialize the sql command and the parameter array
-    var sql = "SELECT * FROM Cars"
-    var params = []
+    // COALESCE allows the user to specify some field values but not others
+    var sql = "SELECT * FROM Cars WHERE Car_ID = COALESCE(?, Car_ID) AND Year = COALESCE(?, Year) AND Make = COALESCE(?, Make) AND Model = COALESCE(?, Model) AND Racer_Turbo = COALESCE(?, Racer_Turbo) AND Racer_Supercharged = COALESCE(?, Racer_Supercharged) AND Racer_Performance = COALESCE(?, Racer_Performance) AND Racer_Horsepower = COALESCE(?, Racer_Horsepower) AND Car_Overall = COALESCE(?, Car_Overall) AND Engine_Modifications = COALESCE(?, Engine_Modifications) AND Engine_Performance = COALESCE(?, Engine_Performance) AND Engine_Chrome = COALESCE(?, Engine_Chrome) AND Engine_Detailing = COALESCE(?, Engine_Detailing) AND Engine_Cleanliness = COALESCE(?, Engine_Cleanliness) AND Body_Frame_Undercarriage = COALESCE(?, Body_Frame_Undercarriage) AND Body_Frame_Suspension = COALESCE(?, Body_Frame_Suspension) AND Body_Frame_Chrome = COALESCE(?, Body_Frame_Chrome) AND Body_Frame_Detailing = COALESCE(?, Body_Frame_Detailing) AND Body_Frame_Cleanliness = COALESCE(?, Body_Frame_Cleanliness) AND Mods_Paint = COALESCE(?, Mods_Paint) AND Mods_Body = COALESCE(?, Mods_Body) AND Mods_Wrap = COALESCE(?, Mods_Wrap) AND Mods_Rims = COALESCE(?, Mods_Rims) AND Mods_Interior = COALESCE(?, Mods_Interior) AND Mods_Other = COALESCE(?, Mods_Other) AND Mods_ICE = COALESCE(?, Mods_ICE) AND Mods_Aftermarket = COALESCE(?, Mods_Aftermarket) AND Mods_WIP = COALESCE(?, Mods_WIP) AND Mods_Overall = COALESCE(?, Mods_Overall)"
+    var params = [req.query.carid, req.query.year, req.query.make, req.query.model, req.query.racerturbo, req.query.racersupercharged, req.query.racerperformance, req.query.racerhorsepower, req.query.caroverall, req.query.enginemodifications, req.query.engineperformance, req.query.enginechrome, req.query.enginedetailing, req.query.enginecleanliness, req.query.bfundercarriage, req.query.bfsuspension, req.query.bfchrome, req.query.bfdetailing, req.query.bfcleanliness, req.query.modspaint, req.query.modsbody, req.query.modswrap, req.query.modsrims, req.query.modsinterior, req.query.modsother, req.query.modsice, req.query.modsaftermarket, req.query.modswip, req.query.modsoverall]
 
     db.all(sql, params, (err, rows) => {
 
@@ -49,11 +59,15 @@ app.get("/api/cars", (req, res, next) => {
 });
 
 //endpoint to get a list of owners in the Owners table
+/*
+    Car_ID, Name, and Email can optionally be specified to display only records that meet certain criteria
+*/
 app.get("/api/owners", (req, res, next) => {
 
     // initialize the sql command and the parameter array
-    var sql = "SELECT * FROM Owners"
-    var params = []
+    // COALESCE allows the user to specify some field values but not others
+    var sql = "SELECT * FROM Owners WHERE Car_ID = COALESCE(?, Car_ID) AND Name = COALESCE(?, Name) AND Email = COALESCE(?, Email)"
+    var params = [req.query.carid, req.query.name, req.query.email]
 
     db.all(sql, params, (err, rows) => {
 
@@ -65,8 +79,8 @@ app.get("/api/owners", (req, res, next) => {
 
         // successful execution
         res.json({
-            "message":"success",
-            "data":rows
+            "message": "success",
+            "data": rows
         })
     });
 });
@@ -327,7 +341,7 @@ app.patch("/api/owners/:carid", (req, res, next) => {
     }
 
     // initialize the sql command and the parameter array
-    // COALSCE allows the user to update some fields and leave others with their initial values
+    // COALESCE allows the user to update some fields and leave others with their initial values
     var sql = `UPDATE Owners SET Name = COALESCE(?, Name), Email = COALESCE(?, Email) WHERE Car_ID = ?`;
     var params = [data.name, data.email, data.carid];
 
