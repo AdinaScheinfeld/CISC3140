00fb23d63348121ea6d4251c304a8fdd68382553
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-05-01 20:20:11 -0400

    Fix formatting of a long string

--- lab4_files/server.js
+++ lab4_files/server.js
@@ -144,7 +144,11 @@ app.get("/api/owners/:name", (req, res, next) => {
 app.post("/api/cars/", (req, res, next) => {
 
     // initialize the sql command, the parameter array, and a coutner fo rhte number of objects to insert
-    var sql = `INSERT INTO Cars (Car_ID, Year, Make, Model, Racer_Turbo, Racer_Supercharged, Racer_Performance, Racer_Horsepower, Car_Overall, Engine_Modifications, Engine_Performance, Engine_Chrome, Engine_Detailing, Engine_Cleanliness, Body_Frame_Undercarriage, Body_Frame_Suspension, Body_Frame_Chrome, Body_Frame_Detailing, Body_Frame_Cleanliness, Mods_Paint, Mods_Body, Mods_Wrap, Mods_Rims, Mods_Interior, Mods_Other, Mods_ICE, Mods_Aftermarket, Mods_WIP, Mods_Overall) 
+    var sql = `INSERT INTO Cars (Car_ID, Year, Make, Model, 
+        Racer_Turbo, Racer_Supercharged, Racer_Performance, Racer_Horsepower, 
+        Car_Overall, Engine_Modifications, Engine_Performance, Engine_Chrome, Engine_Detailing, Engine_Cleanliness, 
+        Body_Frame_Undercarriage, Body_Frame_Suspension, Body_Frame_Chrome, Body_Frame_Detailing, Body_Frame_Cleanliness, 
+        Mods_Paint, Mods_Body, Mods_Wrap, Mods_Rims, Mods_Interior, Mods_Other, Mods_ICE, Mods_Aftermarket, Mods_WIP, Mods_Overall) 
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
     var params = []
     let numObjsToInsert = 0;
