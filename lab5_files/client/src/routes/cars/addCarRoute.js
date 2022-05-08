401518af93ffe26bb9d2bad7da739309f10fb532
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-05-08 17:27:23 -0400

    Add functionality to add a car

---
+++ lab5_files/client/src/routes/cars/addCarRoute.js
@@ -0,0 +1,10 @@
+import AddCar from "../../components/cars/addCar"
+
+// route to add an owner
+export default function AddCarRoute() {
+    return (
+        <div>
+            <AddCar />
+        </div>
+    )
+}
