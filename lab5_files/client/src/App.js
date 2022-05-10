92fd6311a03c59fa22624eba81218d052b86f4bf
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-05-10 17:36:14 -0400

    Add styling to homepage

--- lab5_files/client/src/App.js
+++ lab5_files/client/src/App.js
@@ -1,4 +1,8 @@
+import "../src/App.css"
+
 import Navbar from "./components/navigation/navbar";
+
+import homeCar from "./images/homeCar.jpg"
 
 export default function App() {
   
@@ -6,7 +10,15 @@ export default function App() {
 
     <div>
       <Navbar />
-      <h1>Home Page</h1>
+        <div className="homeOuterContainer">
+          <div className="homeInnerContainer">
+            <h1 className="homeHeader">Home Page</h1>
+            <p className="homeP">Please use the links in the navigation bar above to view, add to, and update the cars data and owners data in the database.</p>
+            <div className="homeImgBox">
+              <img src={homeCar} alt="car" className="homeImg"/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
