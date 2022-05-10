7b4130039971b7eb7fd66dd93d3c10fdf55423e2
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-05-10 17:11:41 -0400

    Update cars and owners homepages

--- lab5_files/client/src/components/cars/carsPage.js
+++ lab5_files/client/src/components/cars/carsPage.js
@@ -1,5 +1,7 @@
 import '../../stylesheets/cars/carsPage.css'
 
+import carsImage from "../../images/cars.jpg"
+
 import Navbar from "../navigation/navbar"
 import CarsNavbar from "../navigation/carsNavbar"
 
@@ -12,7 +14,15 @@ export default function CarsPage() {
         <div>
             <Navbar />
             <CarsNavbar />
-            <h1>Cars Homepage</h1>
+            <div className='outerContainer'>
+                <div className='innerContainer'>
+                    <h1 className='homepageHeader'>Cars Homepage</h1>
+                    <p>Please use the links in the navigation bar above to view the cars in the database, to add a car to the database, or to update a car already in the database.</p>
+                    <div className='imgBox'>
+                        <img src={carsImage} alt="cars" />
+                    </div>
+                </div>
+            </div>
         </div>
   )
 }
--- lab5_files/client/src/components/owners/ownersPage.js
+++ lab5_files/client/src/components/owners/ownersPage.js
@@ -1,5 +1,7 @@
 import '../../stylesheets/owners/ownersPage.css'
 
+import ownersImage from "../../images/owners.jpg"
+
 import Navbar from "../navigation/navbar"
 import OwnersNavbar from "../navigation/ownersNavbar"
 
@@ -12,7 +14,16 @@ export default function OwnersPage() {
         <div>
             <Navbar />
             <OwnersNavbar />
-            <h1>Owners Homepage</h1>
+            <div className='outerContainer'>
+                <div className='innerContainer'>
+                    <h1 className='homepageHeader'>Owners Homepage</h1>
+                    <p>Please use the links in the navigation bar above to view the owners in the database, to add an owner to the database, or to update an owner already in the database.</p>
+                    <div className='imgBox'>
+                        <img src={ownersImage} alt="cars" />
+                    </div>
+                </div>
+            </div>
+
         </div>
   )
 }
